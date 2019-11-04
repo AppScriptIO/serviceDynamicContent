482679f94893989f14e4c62433383f31cd995d09
Safi <nassarsafi@gmail.com>
2019-11-05 00:46:46 +0200

    🐞 Fix babel transpiler middleware.

--- source/middleware/babelTranspiler.js
+++ source/middleware/babelTranspiler.js
@@ -13,7 +13,7 @@ try {
 
 // transpile only on development and non-distribution folders, i.e. on-the-fly transpilation middleware is executed only in development, while in productios the distribution folder should be already transpiled.
 export const transformJavascriptMiddleware = () => async (context, next) => {
-  if (context.response.type == `application/javascript`) context.body = transformJavascript({ scriptCode: context.body })
+  if (context.response.type == `application/javascript`) context.body = await transformJavascript({ scriptCode: context.body })
   await next()
 }
 
