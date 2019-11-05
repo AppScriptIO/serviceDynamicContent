a55477b21adfaa961926d37d3037e9aaa3c0c5d3
Safi <nassarsafi@gmail.com>
2019-11-06 01:39:39 +0200

    ⚗ Grpah data files - split content delivery data from root content rendering data.

---
+++ resource/frontEndSettings.js
@@ -0,0 +1,102 @@
+async function loadFrontendData({ targetConfig }) {
+  let defaultLanguage = 'English'
+  targetConfig.frontendStatic = {
+    // Configurations passed to frontend
+    config: targetConfig,
+    setting: {
+      location: {
+        routeBasePath: `${targetConfig.PROTOCOL}${targetConfig.HOST}`,
+        cdnBasePath: instnace['StaticContent'].url,
+      },
+      mode: {
+        // version / mode of app
+        language: defaultLanguage, // default language
+      },
+    },
+    route: 'route',
+    // documentFrontendData
+    document: [
+      {
+        key: 'registration-single',
+        layout: 'webapp-layout-toolbar',
+        page: {
+          selectorName: 'registration-single',
+          file: 'registration-single.html',
+        },
+      },
+      {
+        key: 'registration-agency',
+        layout: 'webapp-layout-toolbar',
+        page: {
+          selectorName: 'registration-agency',
+          file: 'registration-agency.html',
+        },
+      },
+      {
+        key: 'view-state404',
+        layout: 'webapp-layout-toolbar',
+        page: {
+          selectorName: 'viewState404',
+          file: 'view-state404/view-state404.html$',
+        },
+      },
+      {
+        key: 'universityPage',
+        layout: 'webapp-layout-toolbar',
+        page: {
+          selectorName: 'universityPage',
+          file: 'view-underconstruction.html',
+        },
+      },
+      {
+        key: 'studyfieldPage',
+        layout: 'webapp-layout-toolbar',
+        page: {
+          selectorName: 'studyfieldPage',
+          file: 'view-underconstruction.html',
+        },
+      },
+      {
+        key: 'countryPage',
+        layout: 'webapp-layout-toolbar',
+        page: {
+          selectorName: 'countryPage',
+          file: 'view-list-item.html',
+        },
+      },
+      {
+        key: 'bucharest',
+        layout: 'webapp-layout-toolbar',
+        page: {
+          selectorName: 'studyfieldSingleArticle',
+          file: 'view-article.html',
+        },
+      },
+      {
+        key: 'medicine',
+        layout: 'webapp-layout-toolbar',
+        page: {
+          selectorName: 'studyfieldSingleArticle',
+          file: 'view-article.html',
+        },
+      },
+      {
+        key: 'frontpage',
+        layout: 'webapp-layout-toolbar',
+        page: {
+          selectorName: 'frontPage',
+          file: 'view-frontpage.html',
+        },
+      },
+      {
+        key: 'about',
+        layout: 'webapp-layout-toolbar',
+        page: {
+          selectorName: 'about',
+          file: 'view-about.html',
+        },
+      },
+    ],
+    uiContent: await queryPatternImplementation({ languageDocumentKey: defaultLanguage }),
+  }
+}
