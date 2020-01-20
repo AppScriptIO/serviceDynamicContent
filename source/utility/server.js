79a88350c6fbd21e1ce014e2711486f05478ee5e
Safi <nassarsafi@gmail.com>
2020-01-20 12:04:55 +0200

    ✒ Update internal status messages for the services.

--- source/utility/server.js
+++ source/utility/server.js
@@ -7,23 +7,22 @@ export async function createHttpServer({ serviceName, port, middlewareArray }) {
   serverKoa.subdomainOffset = 1 // for localhost domain.
   // register middleware
   middlewareArray.forEach(middleware => serverKoa.use(middleware))
-  await new Promise((resolve, reject) =>
-    http
-      .createServer(serverKoa.callback())
-      .listen(port, () => {
-        if (process.send !== undefined) process.send({ level: 'service', serviceName, port, status: 'ready', description: 'Server listening' }) // if process is a forked child process.
-        process.emit('listening')
-        console.log(`☕ ${serviceName} server listening on port ${port}`)
-        resolve()
-      })
-      .on('connection', socket => {
-        // console.info('SOCKET OPENED' + JSON.stringify(socket.address()))
-        // socket.on('end', () => console.info('SOCKET END: other end of the socket sends a FIN packet'))
-        // socket.on('timeout', () => console.info('SOCKET TIMEOUT'))
-        // socket.on('error', error => console.info('SOCKET ERROR: ' + JSON.stringify(error)))
-        // socket.on('close', had_error => console.info('SOCKET CLOSED. Is ERROR ?: ' + had_error))
-      })
-      .setTimeout(0, () => console.log('HTTP server connection socket was timedout (console.log in httpServer.setTimeout)!')),
+  await new Promise(
+    (resolve, reject) =>
+      http
+        .createServer(serverKoa.callback())
+        .listen(port, () => {
+          process.emit('service', { serviceName, port, status: 'ready', description: 'Server listening' })
+          resolve()
+        })
+        .on('connection', socket => {
+          // console.info('SOCKET OPENED' + JSON.stringify(socket.address()))
+          // socket.on('end', () => console.info('SOCKET END: other end of the socket sends a FIN packet'))
+          // socket.on('timeout', () => console.info('SOCKET TIMEOUT'))
+          // socket.on('error', error => console.info('SOCKET ERROR: ' + JSON.stringify(error)))
+          // socket.on('close', had_error => console.info('SOCKET CLOSED. Is ERROR ?: ' + had_error))
+        }),
+    // .setTimeout(0, () => console.log('HTTP server connection socket was timedout (console.log in httpServer.setTimeout)!')),
   )
 
   // if (ssl)
